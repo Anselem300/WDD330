86e156512105bfdc721e27be408683a99ee7f5ef
Anselem300 <Osamuel1@byupathway.edu>
2025-07-02 05:27:12 -0700

    Cart debug completed

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,17 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // 1. Get the current cart array (or an empty array if not found)
+  let cart = getLocalStorage("so-cart") || [];
+
+  // 2. Add the new product to the cart array
+  cart.push(product);
+
+  // 3. Save the updated cart back to localStorage
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
